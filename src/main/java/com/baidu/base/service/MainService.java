c8d007f62f4c4371653587c1bf0bd8bb4da5fc20
lyy12332133 <875558109@qq.com>
2017-12-06 11:56:52 +0800

    2017年12月06日11:56:48

---
+++ src/main/java/com/baidu/base/service/MainService.java
@@ -0,0 +1,12 @@
+package com.baidu.base.service;
+
+import com.baidu.user.domain.User;
+
+/**
+ * Created by dllo on 17/12/6.
+ */
+public interface MainService {
+    User findByUsername(String username);
+
+    User findSingle(User user1);
+}
